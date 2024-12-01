074456daaacafebd5b17c567be90f1f3b11114a4
Josh Yamamoto <joshuayamamoto5@gmail.com>
2024-12-01 16:02:19 -0600

    day 1

---
+++ day1/sol.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+input_raw <- data.frame(x = readLines("input.txt"))
+
+input <- input_raw |> 
+  separate(x, into = c("left", "right"), sep = "\\s+") |> 
+  mutate(left = as.numeric(left), right = as.numeric(right))
+
+# part 1
+input |> 
+  mutate(left = sort(left),
+         right = sort(right),
+         diff = abs(left - right)) |> 
+  summarise(res = sum(diff))
+
+
+# part 2
+right_counts <- table(input$right)
+
+lut <- setNames(as.vector(right_counts), names(right_counts))
+
+input |> 
+  mutate(multiplier = lut[as.character(left)],
+         multiplier = replace_na(multiplier, 0),
+         sim_score = left * multiplier) |> 
+  summarise(res = sum(sim_score))
